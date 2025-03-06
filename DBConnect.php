a0ed8e10cfe3e4506da3c74d3cc04eec513276ed
LaeliSvaad <ls@laelisvaad.com>
2025-03-06 12:03:52 +0100

    classe DBConnect fonctionnelle

---
+++ DBConnect.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Classe qui permet de se connecter à la base de données.
+ * Cette classe est un singleton. Cela signifie qu'il n'est pas possible de créer plusieurs instances de cette classe.
+ * Pour récupérer une instance de cette classe, il faut utiliser la méthode getInstance().
+ */
+
+class DBConnect {
+
+    private static ?DBConnect $instance = null;
+    private ?\PDO $connection = null; // Ajout de "?" pour autoriser null
+    private $error;
+
+    private function __construct() {
+        try {
+            $dsn = "mysql:host=". DB_HOST .";dbname=". DB_NAME .";charset=utf8mb4";
+            $this->connection = new PDO($dsn, DB_USER, DB_PASS);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            $this->error = "Erreur de connexion : " . $e->getMessage();
+            $this->connection = null; 
+        }
+    }
+
+    public static function getInstance(): ?DBConnect {
+        if (self::$instance === null) {
+            self::$instance = new DBConnect();
+        }
+        return self::$instance;
+    }
+
+    public function getPDO(): ?\PDO {
+        if ($this->connection === null) {
+            echo $this->error;
+        }
+        return $this->connection;
+    }
+}
+
+
--- main.php
+++ main.php
@@ -1,5 +1,13 @@
 <?php
-while (true) {
+/*while (true) {
     $line = readline("Entrez votre commande : ");
-    echo "Vous avez saisi : $line\n";
-}
+    if($line === "list")
+        echo "Vous avez saisi : $line\n";
+}  */
+require_once("config.php");
+require_once("DBConnect.php");
+
+$db = DBConnect::getInstance(); // Récupération de l'instance singleton
+$pdo = $db->getPDO(); // Récupération de l'objet PDO
+
+var_dump($pdo);
