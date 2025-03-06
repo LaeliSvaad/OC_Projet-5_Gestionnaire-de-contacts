0b5e0467162ba6279a1c5dd5dfa87be6dd2f160b
LaeliSvaad <ls@laelisvaad.com>
2025-03-06 13:51:02 +0100

    database added: contact.sql

---
+++ Contact.php
@@ -0,0 +1,31 @@
+<?php
+class Contact{
+
+    private int $id;
+    private string $name;
+    private string $email;
+    private string $phoneNumber;
+
+    public function getId(): ?int{
+
+    }
+
+    public function getName(): ?string{
+
+    }
+
+    public function getEmail(): ?string{
+        
+    }
+
+    public function getPhoneNumber(): ?string{
+        
+    }
+
+    /*Cette méthode permet d'afficher un contact*/
+    public function toString(): string{
+
+    }
+
+
+}
---
+++ contact.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : db
+-- Généré le : jeu. 06 mars 2025 à 12:50
+-- Version du serveur : 5.7.44
+-- Version de PHP : 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `my_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `name` varchar(120) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `phone_number` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `contact`
+--
+
+INSERT INTO `contact` (`id`, `name`, `email`, `phone_number`) VALUES
+(1, 'Lisa Valade', 'lisa.valade@orange.fr', '+33643518184'),
+(2, 'Flavie Valade', 'nom.prenom@sdf.sdf', '+33000000000'),
+(3, 'Quelqu\'un D\'autre', 'qqundautre@someone.com', '+33000000000');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
